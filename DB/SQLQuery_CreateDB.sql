043167e2b7311fe4fb744b259e04fd54517e26ef
ncthangg <ncthang2206@gmail.com>
2025-01-06 19:27:34 +0700

    [Update] .sql

--- DB/SQLQuery_CreateDB.sql
+++ DB/SQLQuery_CreateDB.sql
@@ -3,21 +3,19 @@ USE MovieAppDB
 
 -- Bảng User_Status
 CREATE TABLE "User_Status" (
-    "statusId" BIGINT IDENTITY(1,1),
+    "statusId" BIGINT IDENTITY(1,1)  PRIMARY KEY,
     "statusName" NVARCHAR(255) NOT NULL,
-    CONSTRAINT "user_status_statusid_primary" PRIMARY KEY ("statusId")
 );
 
 -- Bảng User_Role
 CREATE TABLE "User_Role" (
-    "roleId" BIGINT IDENTITY(1,1),
+    "roleId" BIGINT IDENTITY(1,1)  PRIMARY KEY,
     "roleName" NVARCHAR(255) NOT NULL,
-    CONSTRAINT "user_role_roleid_primary" PRIMARY KEY ("roleId")
 );
 
 -- Bảng User
 CREATE TABLE "User" (
-    "userId" BIGINT IDENTITY(1,1),
+    "userId" BIGINT IDENTITY(1,1)  PRIMARY KEY,
     "name" NVARCHAR(255) NOT NULL,
     "email" NVARCHAR(255) NOT NULL,
     "passwordhash" NVARCHAR(MAX) NOT NULL,
@@ -26,7 +24,6 @@ CREATE TABLE "User" (
 	"isVerified" BIT NOT NULL DEFAULT 0,
     "createdAt" DATETIME NOT NULL,
     "updatedAt" DATETIME NOT NULL,
-    CONSTRAINT "user_userid_primary" PRIMARY KEY ("userId"),
     CONSTRAINT "user_roleid_foreign" FOREIGN KEY ("roleId") REFERENCES "User_Role"("roleId"),
     CONSTRAINT "user_statusid_foreign" FOREIGN KEY ("statusId") REFERENCES "User_Status"("statusId")
 );
@@ -35,67 +32,121 @@ CREATE UNIQUE INDEX "user_email_unique" ON "User"("email");
 
 -- Bảng Category
 CREATE TABLE "Category" (
-    "categoryId" BIGINT IDENTITY(1,1),
+    "categoryId" BIGINT IDENTITY(1,1)  PRIMARY KEY,
     "categoryName" NVARCHAR(255) NOT NULL,
-    CONSTRAINT "category_categoryid_primary" PRIMARY KEY ("categoryId")
+);
+
+-- Bảng Actor
+CREATE TABLE "Actor" (
+    "actorId" BIGINT IDENTITY(1,1) PRIMARY KEY,
+    "actorName" NVARCHAR(255) NOT NULL,
+    "birthDate" DATE NULL,
+    "bio" NVARCHAR(MAX) NULL
+);
+
+-- Bảng MovieType
+CREATE TABLE "Movie_Type" (
+    "typeId" BIGINT IDENTITY(1,1) PRIMARY KEY,
+    "typeName" NVARCHAR(255) NOT NULL
 );
 
 -- Bảng Movie
 CREATE TABLE "Movie" (
-    "movieId" BIGINT IDENTITY(1,1),
+    "movieId" BIGINT IDENTITY(1,1) PRIMARY KEY,
     "movieName" NVARCHAR(255) NOT NULL,
     "description" NVARCHAR(MAX) NOT NULL,
     "videoUrl" NVARCHAR(MAX) NULL,
     "posterUrl" NVARCHAR(MAX) NULL,
     "director" NVARCHAR(255) NOT NULL,
     "releaseYear" INT NOT NULL,
+	"typeId" BIGINT NOT NULL,
     "createdAt" DATETIME NULL,
     "updatedAt" DATETIME NULL,
 	"isDeleted" BIT NOT NULL DEFAULT 0,
-    CONSTRAINT "movie_movieid_primary" PRIMARY KEY ("movieId")
+	CONSTRAINT "movie_type_typeid_foreign" FOREIGN KEY ("typeId") REFERENCES "Movie_Type"("typeId")
 );
+
+-- Bảng Movie_Actor
+CREATE TABLE "Movie_Actor" (
+    "id" BIGINT IDENTITY(1,1) PRIMARY KEY,
+    "movieId" BIGINT NOT NULL,
+    "actorId" BIGINT NOT NULL,
+    CONSTRAINT "movie_actor_movieid_foreign" FOREIGN KEY ("movieId") REFERENCES "Movie"("movieId"),
+    CONSTRAINT "movie_actor_actorid_foreign" FOREIGN KEY ("actorId") REFERENCES "Actor"("actorId")
+);
+
+-- Bảng Season
+CREATE TABLE "Movie_Season" (
+    "seasonId" BIGINT IDENTITY(1,1) PRIMARY KEY,
+    "movieId" BIGINT NOT NULL,
+    "seasonName" NVARCHAR(MAX) NOT NULL,
+	"posterUrl" NVARCHAR(MAX) NULL,
+    "releaseYear" INT NOT NULL,
+    "createdAt" DATETIME NULL,
+    "updatedAt" DATETIME NULL,
+	"isDeleted" BIT NOT NULL DEFAULT 0,
+	CONSTRAINT "movie_season_movie_foreign" FOREIGN KEY ("movieId") REFERENCES "Movie"("movieId")
+ON DELETE CASCADE,
+);
+
+-- Bảng Episodes
+CREATE TABLE "Movie_Episode" (
+    "episodeId" BIGINT IDENTITY(1,1) PRIMARY KEY,
+    "seasonId" BIGINT NOT NULL,
+    "episodeName" NVARCHAR(MAX) NOT NULL,
+	"description" NVARCHAR(MAX) NOT NULL,
+    "videoUrl" NVARCHAR(MAX) NULL,
+    "posterUrl" NVARCHAR(MAX) NULL,
+    "createdAt" DATETIME NULL,
+    "updatedAt" DATETIME NULL,
+	"isDeleted" BIT NOT NULL DEFAULT 0,
+	CONSTRAINT "movie_episode_movie_season_foreign" FOREIGN KEY ("seasonId") REFERENCES "Movie_Season"("seasonId")
+ON DELETE CASCADE,
+);
+
 -- Bảng Movie_Category
 CREATE TABLE "Movie_Category" (
-    "id" BIGINT IDENTITY(1,1),
+    "id" BIGINT IDENTITY(1,1) PRIMARY KEY ,
     "movieId" BIGINT NOT NULL,
     "categoryId" BIGINT NOT NULL,
-    CONSTRAINT "movie_category_id_primary" PRIMARY KEY ("id"),
     CONSTRAINT "movie_category_movieid_foreign" FOREIGN KEY ("movieId") REFERENCES "Movie"("movieId"),
     CONSTRAINT "movie_category_categoryid_foreign" FOREIGN KEY ("categoryId") REFERENCES "Category"("categoryId")
 );
 
 -- Bảng Movie_Rate
 CREATE TABLE "Movie_Rate" (
-    "id" BIGINT IDENTITY(1,1),
+    "id" BIGINT IDENTITY(1,1) PRIMARY KEY,
     "movieId" BIGINT NOT NULL,
     "userId" BIGINT NOT NULL,
     "vote" BIGINT CHECK ("vote" BETWEEN 1 AND 5),
     "comment" NVARCHAR(MAX) NULL,
 	"createdAt" DATETIME NULL,
     "updatedAt" DATETIME NULL,
-    CONSTRAINT "movie_rate_id_primary" PRIMARY KEY ("id"),
     CONSTRAINT "movie_rate_movieid_foreign" FOREIGN KEY ("movieId") REFERENCES "Movie"("movieId"),
     CONSTRAINT "movie_rate_userid_foreign" FOREIGN KEY ("userId") REFERENCES "User"("userId")
 );
 
 -- Bảng User_WatchHistory
 CREATE TABLE "User_WatchHistory" (
-    "id" BIGINT IDENTITY(1,1),
+    "id" BIGINT IDENTITY(1,1) PRIMARY KEY,
     "userId" BIGINT NOT NULL,
     "movieId" BIGINT NULL,
-    "watchAt" DATETIME NULL,
-    CONSTRAINT "user_watchhistory_id_primary" PRIMARY KEY ("id"),
+	"seasonId" BIGINT NULL,
+	"episodeId" BIGINT NULL,
+    "lastWatch" DATETIME NULL,
+	"timeWatch" BIGINT NULL,
     CONSTRAINT "user_watchhistory_userid_foreign" FOREIGN KEY ("userId") REFERENCES "User"("userId"),
-    CONSTRAINT "user_watchhistory_movieid_foreign" FOREIGN KEY ("movieId") REFERENCES "Movie"("movieId")
+    CONSTRAINT "user_watchhistory_movieid_foreign" FOREIGN KEY ("movieId") REFERENCES "Movie"("movieId"),
+	CONSTRAINT "user_watchhistory_seasonId_foreign" FOREIGN KEY ("seasonId") REFERENCES "Movie_Season"("seasonId"),
+	CONSTRAINT "user_watchhistory_episodeId_foreign" FOREIGN KEY ("episodeId") REFERENCES "Movie_Episode"("episodeId")
 );
 
 -- Bảng User_Like
 CREATE TABLE "User_Like" (
-    "id" BIGINT IDENTITY(1,1),
+    "id" BIGINT IDENTITY(1,1) PRIMARY KEY,
     "userId" BIGINT NOT NULL,
     "movieId" BIGINT NULL,
     "createdAt" DATETIME NULL,
-    CONSTRAINT "user_like_id_primary" PRIMARY KEY ("id"),
     CONSTRAINT "user_like_userid_foreign" FOREIGN KEY ("userId") REFERENCES "User"("userId"),
     CONSTRAINT "user_like_movieid_foreign" FOREIGN KEY ("movieId") REFERENCES "Movie"("movieId")
 );
@@ -120,3 +171,18 @@ CREATE TABLE "User_Token" (
     "lastLogin" DATETIME NULL,
     CONSTRAINT "user_token_userid_foreign" FOREIGN KEY ("userId") REFERENCES "User"("userId") ON DELETE CASCADE
 );
+
+drop table Actor
+drop table Category
+drop table Movie_Category
+drop table Movie
+drop table Movie_Type
+drop table Movie_Season
+drop table Movie_Episode
+drop table Movie_Rate
+drop table [User]
+drop table User_Like
+drop table User_Role
+drop table User_Status
+drop table User_Token
+drop table User_WatchHistory
